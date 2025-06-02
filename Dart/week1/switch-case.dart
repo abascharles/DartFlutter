14a0532c3b64e6c84ca2e2e99c37d14a522243aa
abascharles <abascharlesbenard@gmail.com>
2025-06-02 11:12:52 +0300

    Switch case statements

---
+++ Dart/week1/switch-case.dart
@@ -0,0 +1,17 @@
+//Switch case - used to evaluate expressions and execute diffrent blocks of code based on matching cases
+void main() {
+  int day = 0;
+  switch (day) {
+    case 1:
+      print("🌞 Monday: Let's code!");
+      break;
+    case 2:
+      print("🚀 Tuesday: Keep coding!");
+      break;
+    case 3:
+      print("🐪 Wednesday: Halfway through!");
+      break;
+    default:
+      print("🎉 Ladies and getlement the weekend is here!");
+  }
+}
