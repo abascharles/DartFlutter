471e55ae3e3ba377e91b900e23f8827638c36790
abascharles <abascharlesbenard@gmail.com>
2025-06-02 09:47:42 +0300

    Lists- or let's say array

---
+++ Dart/week1/lists.dart
@@ -0,0 +1,12 @@
+//Lists - ordered collection of items - arrays in Js i.e shopping list
+void main() {
+  List numbers = [0, 1, 2, 3, 4, 5];
+
+  //Adding value to the list
+  numbers.add(67);
+  print(numbers);
+
+  //Removing values from the list
+  numbers.remove(4);
+  print(numbers);
+}
