5cda3612a54a5d51a580d61d5d2426dc27928705
abascharles <abascharlesbenard@gmail.com>
2025-06-02 09:58:31 +0300

    Maps

---
+++ Dart/week1/maps.dart
@@ -0,0 +1,5 @@
+//Maps - Dynamic collection that represents a set of values as key value pairs - Objects
+void main() {
+  Map<String, int> students = {'Abas': 25, 'Chela': 20};
+  print(students);
+}
