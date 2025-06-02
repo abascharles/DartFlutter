155e8b835532d8ab91e4d4c28340a669c99a4908
abascharles <abascharlesbenard@gmail.com>
2025-06-02 10:43:41 +0300

    control flow - if else statements

---
+++ Dart/week1/ife-else.dart
@@ -0,0 +1,10 @@
+//if statements - used to make decisions based on conditions
+
+void main() {
+  var age = 11;
+  if (age >= 18) {
+    print('I can vote in Kenya!');
+  } else {
+    print('I cannot Vote in Kenya!');
+  }
+}
