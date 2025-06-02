41c4ea72fa3fd51ee5242b1c624d4d8d43f7b27f
abascharles <abascharlesbenard@gmail.com>
2025-06-02 09:46:21 +0300

    Booleans

--- Dart/week1/booleans.dart
+++ Dart/week1/booleans.dart
@@ -1,6 +1,13 @@
 void main() {
   //Boolean - True or false values
-  bool isMarried = true;
+  bool isMarried = false;
   print(isMarried);
   print(10 > 30);
+
+  //Used also in making decisions
+  if (isMarried) {
+    print("Get ready! Get ready!");
+  } else {
+    print("Get a wife!");
+  }
 }
