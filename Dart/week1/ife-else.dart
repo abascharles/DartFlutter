e9b888502186cdd569e517b92ddd87d6676f2845
abascharles <abascharlesbenard@gmail.com>
2025-06-02 10:52:26 +0300

    if-else

--- Dart/week1/ife-else.dart
+++ Dart/week1/ife-else.dart
@@ -4,7 +4,9 @@ void main() {
   var age = 11;
   if (age >= 18) {
     print('Wantam');
+  } else if (age == 10) {
+    print('No Term');
   } else {
-    print('TwoTerm');
+    print('Two Term');
   }
 }
