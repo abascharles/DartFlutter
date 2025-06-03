e86d0de1372e51a2e7c685ed8ff4c73451dc1610
abascharles <abascharlesbenard@gmail.com>
2025-06-03 09:26:57 +0300

    defined the map to store student data

--- Dart/week1/challange.dart
+++ Dart/week1/challange.dart
@@ -1,3 +1,25 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  Map<String, List<int>> studentData = {};
+
+  for (int i = 1; i <= 5; i++) {
+    print('Enter student $i name:');
+    String name = stdin.readLineSync()!;
+
+    List<int> marks = [];
+    for (int j = 1; j <= 3; j++) {
+      print('Enter mark $j:');
+      int mark = int.parse(stdin.readLineSync()!);
+      marks.add(mark);
+    }
+
+    //Add name add marks to the loop
+    studentData[name] = marks;
+    print('Marks entered for $name: $marks\n');
+  }
+  print('All student data');
+  studentData.forEach((name, marks) {
+    print('$name, $marks');
+  });
+}
