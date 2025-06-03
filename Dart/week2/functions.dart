38df1d7ffd31c7761fa8e18f40cc73a5f35a4a4f
abascharles <abascharlesbenard@gmail.com>
2025-06-03 10:45:16 +0300

    Types of functions

--- Dart/week2/functions.dart
+++ Dart/week2/functions.dart
@@ -1,8 +1,18 @@
 //Functions - block of code that perform specific tasks usefule for resuing code
-void printName() {
-  print('Im Abas Charles');
-}
 
 void main() {
+  //Types of Functions
+  //1. No Parameter and No Return Type: A function that neither takes any input nor returns any value 🤷‍♂️.
+  void printName() {
+    print('Im Spongebob');
+  }
+
   printName();
+  //Parameter and No Return Type: A function that takes inputs but doesn’t give anything back 🎁.
+
+  void printNomen(String name) {
+    print('My name is $name');
+  }
+
+  printNomen("Aron Jackson");
 }
