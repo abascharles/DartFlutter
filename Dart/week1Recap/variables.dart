3e1e11d412f6520fe2fa93a84bb3852befeb04b5
abascharles <abascharlesbenard@gmail.com>
2025-06-07 20:11:52 +0300

    Week one recap

---
+++ Dart/week1Recap/variables.dart
@@ -0,0 +1,40 @@
+void main() {
+  //Control flows - uses to make descisions, loop/repeat over codes
+  //Descision Statements
+
+  // int age = -1;
+
+  // if (age >= 18) {
+  //   print("Wantam is true");
+  // } else if (age <= 0) {
+  //   print("Enter a valid age!!");
+  // } else {
+  //   print("Go and Sleep");
+  // }
+
+  //Switch statements
+  String day = "Thursady";
+
+  switch (day) {
+    case "Monday":
+      print("Spongebob");
+      break;
+    case "Tuesday":
+      print("Squarepants");
+      break;
+    case "Wednesday":
+      print("Patrick");
+      break;
+    case "Thursady":
+      print("Mr crabs");
+      break;
+    case "Friday":
+      print("Bikini Bottom");
+      break;
+    case "Saturday":
+      print("Ladies and gentelemen the weekend!");
+      break;
+    default:
+      print("Go to church");
+  }
+}
